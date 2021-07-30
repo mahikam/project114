f72ad77696065f245fa6a0aa95a02beb0d0af6cf
mahikam <76594479+mahikam@users.noreply.github.com>
2021-07-30 15:37:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){};
+
+function setup(){
+    canvas= createCanvas(400,400)
+    canvas.position(475,175);
+}
+
+function draw(){};
+
+function take_snapshot(){
+    save("project114.png");
+}
